cc88b7ee51db9743a2dab134b87aa1fac12e8486
Luis Hernandez <luis.hernandez1@ucalgary.ca>
2025-08-31 00:18:31 -0600

    cleaning data

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello world")
+import pandas as pd
+from sklearn.ensemble import RandomForce
+
+# getting file
+matches = pd.read_csv('season-2425.csv')
+
+# convert date collum into a date object
+matches["Date"] = pd.to_datetime(matches["Date"])
+
+# Adding a day of the week
+matches["Day_Code"] = matches["Date"].dt.dayofweek
+
+# Results
+matches["Target"] = (matches["FTR"] == "H").astype('int')
